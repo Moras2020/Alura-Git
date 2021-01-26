16b65bdcdde8ba50f074d4ea1130da584213b4c9
Ezequiel de Jesus morais <ezequiel-j-m@hotmail.com>
2021-01-26 12:52:15 -0300

    adicionando um novo curso

--- Programa.java
+++ Programa.java
@@ -6,7 +6,7 @@ public class Programa {
 		
 		
 		public static void main(String [] arg) { 
-		System.out.println("adicionando um no curso Branch");
+		System.out.println("adicionando um novo curso Branch");
 		
 		}
 	}
